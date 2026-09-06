8640a62f8c1656c8520eafae0ccf485a9e26900c
wii <lomugdanglf.19@gmail.com>
2026-09-06 16:49:32 +0800

    assign unique dev ports per app

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'] });
+  app.enableCors({ origin: ['http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
