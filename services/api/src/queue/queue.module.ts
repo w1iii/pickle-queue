c6a7fffb83d01fcfafb393809eeb8a7e56a922ac
David Joshua Albos <your.email@example.com>
2026-09-07 14:26:03 +0800

    added QR

--- services/api/src/queue/queue.module.ts
+++ services/api/src/queue/queue.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatchingService } from './matching.service.js';
+import { QrModule } from './qr/qr.module.js';
 import { QueueController } from './queue.controller.js';
 import { QueueService } from './queue.service.js';
 
 @Module({
+  imports: [QrModule],
   controllers: [QueueController],
   providers: [QueueService, MatchingService],
   exports: [QueueService, MatchingService],
